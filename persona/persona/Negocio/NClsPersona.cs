50609767651ebe132f05928bbf89cebff9a07e16
Rkevjoker <88000934+Rkevjoker@users.noreply.github.com>
2021-08-16 20:21:08 -0600

    actualizacion2Rk

--- persona/persona/Negocio/NClsPersona.cs
+++ persona/persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace persona.Negocio
 {
     class NClsPersona
     {
+        public String Hablar(ClsPersona persona)
+        {
+            persona.Nombre = "Martin";
+
+            return "la persona" + persona.Nombre + "esta hablando";
+        }
+        public String Caminar(ClsPersona persona)
+        {
+            persona.Nombre = "Martin";
+            return "la persona llamada" + persona.Nombre + " esta caminando";
+        }
+        public String Reir(ClsPersona persona)
+        {
+            persona.Nombre = "Martin";
+            return "la persona de nombre" + persona.Nombre + "se esta riendo";
+        }
+        public String Observar(ClsPersona persona)
+        {
+            persona.Nombre = "Martin";
+            return "la persona llamada" + persona.Nombre + "te esta observando";
+
+        }
     }
 }
