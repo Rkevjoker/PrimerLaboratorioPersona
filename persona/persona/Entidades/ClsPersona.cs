da76c04723236e84e78f7ba2952b25bf705d07a1
jeffer2020 <rc0113032021@unab.edu.sv>
2021-08-16 19:59:37 -0600

    persona

--- persona/persona/Entidades/ClsPersona.cs
+++ persona/persona/Entidades/ClsPersona.cs
@@ -8,5 +8,14 @@ namespace persona.Entidades
 {
     class ClsPersona
     {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string sexo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
     }
 }
